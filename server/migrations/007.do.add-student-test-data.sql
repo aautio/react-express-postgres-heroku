75cf8b4676cdf99b6653e7a00704a9fb7110b703
catai <catai@edu.uwaterloo.ca>
2020-10-07 20:35:46 -0400

    Removed all the backend demo work and restored branch to its empty state.

--- server/migrations/007.do.add-student-test-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Student (student_id, first_name, last_name, email, password, phone, school_id) 
-VALUES (12345678, 'John', 'Doe', 'johndoe@gmail.com', 'a1234', NULL, 1);
--- server/routes/public.js
+++ server/routes/public.js
@@ -1,13 +1,5 @@
 const express = require('express')
 
-const authenticateHandler = require('../models/handlers/authenticate');
-
 const router = express.Router();
-
-router.get('/api/login', async (req, res) => {
-    const params = req.query;
-    const response = await authenticateHandler.isUserCredentialsValid(params.email, params.password);
-    res.send(response);
-});
 
 module.exports = router
